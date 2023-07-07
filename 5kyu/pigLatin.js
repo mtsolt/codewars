047660cf4748074169df14351bc361a465e38460
Mike Soltysiak <mtsolt@outlook.com>
2023-07-08 08:43:46 +1000

    final function - as well as better solutions from codewars

--- 5kyu/pigLatin.js
+++ 5kyu/pigLatin.js
@@ -6,22 +6,52 @@ function pigIt(str) {
   const allowed = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
   let newStr = str.split(' ')
   let newLength = newStr.length
+  let finalStr = []
+  
   for (let i = 0; i < newLength; i++) {
     let newWord = newStr[i]
-    console.log('THis is new word', newWord)
+    
     if (allowed.includes(newWord.charAt(0))) {
-      console.log('This is newWord[i]', newStr[i])
+      let ending = newWord.charAt(0)
+      let splitArr = newWord.split('')
+      splitArr.shift()
+      splitArr.push(ending, "ay")
+      let joinArr = splitArr.join('')
+      finalStr.push(joinArr)
     } else {
-      console.log('BLAH')
+      finalStr.push(newWord)
     }
+    
+    
   }
-
-    console.log('End result >>>',newStr)
-
-    // return str
+  return jumbo = finalStr.join(' ')
 
 
   }
-  pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
-  pigIt('Hello world !');     // elloHay orldway !
+//   console.log(pigIt('Pig latin is cool !')); // igPay atinlay siay oolcay
+//   // pigIt('Hello world !');     // elloHay orldway !
 
+
+
+  // BETTER VERSIONS FROM GITHUB
+  // function pigIt(str){
+  //   return str.replace(/(\w)(\w*)(\s|$)/g, "\$2\$1ay\$3")
+  // }
+
+  // function pigIt(str) {
+  //   return str.replace(/\w+/g, (w) => {
+  //     return w.slice(1) + w[0] + 'ay';
+  //   });
+  // }
+  // *For these two I clearly need to work on looking at how replace works! - These do work with symbols. 
+
+  // function pigIt(str) {
+  //   var arrayWord = str.split(' ');
+  //   return arrayWord.map(function(word) {
+  //     var firstLetter = word.charAt(0);
+  //     return word.slice(1) + firstLetter + 'ay';
+  //   }).join(' ');
+  // }
+  console.log(pigIt('Pig latin is cool !'));
+// RETURNS igPay atinlay siay oolcay !ay
+  // * For this one I'm not entirely sure how it would work with symbols. - Have tested it and it doesn't work - I don't like it./
