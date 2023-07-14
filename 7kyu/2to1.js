6921abfe8679285cf7275adb5ac19b33d6135937
Mike Soltysiak <mtsolt@outlook.com>
2023-07-15 08:33:08 +1000

    2to1 solved and clever solutions added too.

--- 7kyu/2to1.js
+++ 7kyu/2to1.js
@@ -2,9 +2,42 @@
 // Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
 
 
-// a = "xyaabbbccccdefww"
-// b = "xxxxyyyyabklmopq"
-// longest(a, b) -> "abcdefklmopqwxy"
+function longest(a, b) {
+  let final = []
+  function sorter(c) {
+    let cSplit = c.split('')
+    for (let i = 0; i<cSplit.length; i++){
+      !final.includes(cSplit[i]) ? final.push(cSplit[i])
+      : null
+    }
+  }
+  sorter(a)
+  sorter(b)
+  final.sort()
+  let result = final.join('')
+  console.log(result)
+}
 
-// a = "abcdefghijklmnopqrstuvwxyz"
-// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) //-> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) // "abcdefghijklmnopqrstuvwxyz"
+
+
+
+// * Best solutions from Codewars
+// const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
+
+// function longest(s1, s2) {
+//   s3 = s1 + s2;
+//   s4 = s3.split("");
+//   s4 = s4.sort().filter(function(element, index, array){
+//     return element !== array[index - 1];
+//   });
+//   return s4.join("");
+// }
+
+// * REMEMBER - Easy to add two strings together with + && - also Sets
